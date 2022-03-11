7dd060537a1b0cfd51b66d37b2ba98df8f7347c1
GuFFy_OwO <golovin.mv.vl5@gmail.com>
2022-03-11 17:46:52 +0500

    Optimization

--- main.py
+++ main.py
@@ -1,53 +1,83 @@
-import vk_api, requests, re, os, time
 from sys import exit
 
-def cls():
-    os.system("cls" if os.name=="nt" else "clear")
+import os
+import re
+import requests
+import time
+import vk_api
 
-def log_in():
+
+def cls():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def main():
     if not os.path.exists("data"):
         os.makedirs("data")
-    global dl
-    global all
-    dl = 0
-    all = 0
+
+    if os.path.exists("vk_config.v2.json"):
+        os.remove("vk_config.v2.json")
+
     cls()
+
     login_str = input("Login: ")
     password_str = input("Password: ")
-    vkObj = vk_api.VkApi(login=login_str, password=password_str, auth_handler=auth_handler)
+    vk_obj = vk_api.VkApi(login=login_str, password=password_str, auth_handler=auth_handler)
     try:
-        vkObj.auth()
+        vk_obj.auth()
     except vk_api.AuthError as error_msg:
         print(error_msg)
         time.sleep(2)
-        log_in()
+        main()
     else:
         print("Success login!")
-    api = vkObj.get_api()
+        documents_downloader(vk_obj)
+
+
+def documents_downloader(vk_obj):
+    api = vk_obj.get_api()
     documents = api.docs.get()["items"]
-    all = len(documents)
+
+    downloaded = 0
+    all_doc_count = len(documents)
+
     for i in range(len(documents)):
-        el = documents[i]
-        r = requests.get(el["url"])
+        downloaded += 1
+        document = documents[i]
+
+        try:
+            r = requests.get(document["url"])
+        except:
+            continue
+
         if r.status_code == 200:
-            with open("data/" + get_valid_filename(str(el["id"]) + "_" + el["title"]), "wb") as output_file:
+            with open("data/" + get_valid_filename(str(document["id"]) + "_" + document["title"]), "wb") as output_file:
                 output_file.write(r.content)
-        dl += 1
-        stat()
+
+        stat(downloaded, all_doc_count)
+
+    if os.path.exists("vk_config.v2.json"):
+        os.remove("vk_config.v2.json")
+
     input("Press Enter to exit.")
     exit()
+
 
 def auth_handler():
     key = input("Enter authentication code: ")
     remember_device = True
     return key, remember_device
 
+
 def get_valid_filename(s):
     s = str(s).strip().replace(' ', '_')
     return re.sub(r'(?u)[^-\w.]', '', s)
 
-def stat():
-    cls()
-    print(f"Downloaded: {dl}/{all}")
 
-log_in()
+def stat(downloaded, all_doc_count):
+    cls()
+    print(f"Downloaded: {downloaded}/{all_doc_count}")
+
+
+if __name__ == '__main__':
+    main()
